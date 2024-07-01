6044314b19d07a1822c03b61ea8aa1b5b805e433
常东东 <>
2024-07-01 19:50:38 +0800

    update

---
+++ resume/assets/index-pwJ4p8kN.js
@@ -0,0 +1,8 @@
+import{a as n,b as i,_ as s}from"./vue-vendor-Jvnp4JD5.js";import{i as l}from"./locales-Re8IwkW1.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const _ of document.querySelectorAll('link[rel="modulepreload"]'))t(_);new MutationObserver(_=>{for(const e of _)if(e.type==="childList")for(const r of e.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&t(r)}).observe(document,{childList:!0,subtree:!0});function c(_){const e={};return _.integrity&&(e.integrity=_.integrity),_.referrerPolicy&&(e.referrerPolicy=_.referrerPolicy),_.crossOrigin==="use-credentials"?e.credentials="include":_.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function t(_){if(_.ep)return;_.ep=!0;const e=c(_);fetch(_.href,e)}})();function f(){console.log(`%c_______  __   __  _______    ______    _______  _______  __   __  __   __  _______ 
+%c|       ||  | |  ||       |  |    _ |  |       ||       ||  | |  ||  |_|  ||       |
+%c|    ___||  |_|  ||  _____|  |   | ||  |    ___||  _____||  | |  ||       ||    ___|
+%c|   |___ |       || |_____   |   |_||_ |   |___ | |_____ |  |_|  ||       ||   |___ 
+%c|    ___||_     _||_____  |  |    __  ||    ___||_____  ||       ||       ||    ___|
+%c|   |      |   |   _____| |  |   |  | ||   |___  _____| ||       || ||_|| ||   |___ 
+%c|___|      |___|  |_______|  |___|  |_||_______||_______||_______||_|   |_||_______|
+   `,"color:#ff0000","color:#ff7500","color:#FFAD00","color:#FEDA00","color:#D0FD00","color:#00FF3B","color:#00FFB1"),console.log("https://github.com/Fridolph/my-resume/","如果您感兴趣或者对您有帮助，麻烦给个STAR吧，谢谢！~")}const u=n(),a=i(s);a.use(u).use(l).mount("#app");f();
